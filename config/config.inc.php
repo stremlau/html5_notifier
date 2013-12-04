57542f3f507f2d795e478d032f126595cff651f3
Nicolas Joubert <webmaster@nvirenque.com>
2013-12-04 22:35:46 +0100

    Add ability to exclude some directory of notifications

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,5 +1,6 @@
 <?php
 /* defaults */
 $rcmail_config['html5_notifier_duration'] = '3'; //Display duration of Desktop Notifications (0 = disabled)
-$rcmail_config['html5_notifier_smbox'] = '1'; //Lenght of displayed mailboxname 2=max 1=short 0=do not show
+$rcmail_config['html5_notifier_smbox'] = '1'; //Length of displayed mailboxname 2=max 1=short 0=do not show
+$rcmail_config['html5_notifier_excluded_directories'] = 'Trash' //Directories excluded for notifications. Use ; as separator for multiple directories;
 ?>
