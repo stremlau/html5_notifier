3e97a8e3c4863e89d5f59665c277018ef2d85d6f
Iblis Lin <e196819@hotmail.com>
2013-12-15 01:23:08 +0800

    Add gitignore for configuration file

--- config/config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* defaults */
-$rcmail_config['html5_notifier_duration'] = '3'; //Display duration of Desktop Notifications (0 = disabled)
-$rcmail_config['html5_notifier_smbox'] = '1'; //Lenght of displayed mailboxname 2=max 1=short 0=do not show
-?>
